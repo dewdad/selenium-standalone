13a2b0e5177424d99adb050209d8b3e7987cbd05
Avital Ben-Natan <avital.ben-natan@sap.com>
2014-03-23 16:44:33 +0200

    Changed to use http_proxy env variable

--- setup_proxy.js
+++ setup_proxy.js
@@ -1,26 +1,60 @@
-if (!process.env.http_proxy) return;
+var _debug;
+if (!!process.env.http_proxy){
+    var url = require('url');
+    var proxy = url.parse(process.env.http_proxy); // {hostname:'', port:''}
 
-var url    = require('url');
-var tunnel = require('tunnel');
-var proxy = url.parse(process.env.http_proxy);
+    var https = require('https');
+    var oldhttpsreq = https.request;
+    https.request = function (options, callback) {
+        var request;
 
-var tunnelingAgent = tunnel.httpsOverHttp({
-  proxy: {
-    host: proxy.hostname,
-    port: proxy.port
-  }
-});
+        if(_debug) console.log('proxied https request.');
+        var __options = typeof(options)==='string'? url.parse(options): options;
+        __options.path = 'https://' + __options.host + __options.path;
+        __options.host = proxy.host.split(':')[0];
+        __options.port = proxy.port;
+        if (_debug) {
+            console.log('=== https-proxy.js begin debug ===');
+            //console.log(JSON.stringify(__options, null, 2));
+            console.log(__options);
+            console.log('=== https-proxy.js end debug ===');
+        }
+        request = oldhttpsreq.call(null, __options, callback);
+        request.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+        return request;
+    };
 
-https = require('https');
-var oldhttpsreq = https.request;
-https.request = function (options, callback) {
-  options.agent = tunnelingAgent;
-  return oldhttpsreq.call(null, options, callback);
-};
+    var http = require('http');
+    var oldhttpreq = http.request;
+    http.request = function (options, callback) {
+        var request;
 
-http = require('http');
-var oldhttpreq = http.request;
-http.request = function (options, callback) {
-  options.agent = tunnelingAgent;
-  return oldhttpreq.call(null, options, callback);
+        if(_debug) console.log('proxied http request.');
+        var __options = typeof(options)==='string'? url.parse(options): options;
+        __options.path = 'http://' + __options.host + __options.path;
+        __options.host = proxy.host.split(':')[0];
+        __options.port = parseInt(proxy.port);
+        if (_debug) {
+            console.log('=== http-proxy.js begin debug ===');
+            //console.log(JSON.stringify(__options, null, 2));
+            console.log(__options);
+            console.log('=== http-proxy.js end debug ===');
+        }
+        request = oldhttpreq.call(null, __options, function(res){
+            if(_debug) console.log(['callback:', res]);
+            callback(res);
+        });
+        request.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+        return request;
+    };
+}else{
+    console.log('No http_proxy variable found.');
+}
+
+module.exports = function(debug){
+  _debug = debug || false;
 };
