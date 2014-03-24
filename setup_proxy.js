8865a3c9ad3b5b48b09fe9712e9e70d58a619880
Avital Ben-Natan <avital.ben-natan@sap.com>
2014-03-24 11:26:00 +0200

    support for http_proxy env variable

--- setup_proxy.js
+++ setup_proxy.js
@@ -1,7 +1,23 @@
 var _debug;
+
 if (!!process.env.http_proxy){
     var url = require('url');
     var proxy = url.parse(process.env.http_proxy); // {hostname:'', port:''}
+
+    var genOptions = function(options){
+        var __options = typeof(options)==='string'? url.parse(options): options;
+        __options.headers = __options.headers || {};
+        __options.headers.Host = __options.host;
+        __options.path = 'http://' + __options.host + __options.path;
+        __options.hostname = proxy.host.split(':')[0];
+        __options.port = parseInt(proxy.port);
+        if (_debug) {
+            console.log('=== begin proxy request debug ===');
+            console.log(__options);
+            console.log('=== end proxy request debug ===');
+        }
+        return __options;
+    };
 
     var https = require('https');
     var oldhttpsreq = https.request;
@@ -9,16 +25,7 @@ if (!!process.env.http_proxy){
         var request;
 
         if(_debug) console.log('proxied https request.');
-        var __options = typeof(options)==='string'? url.parse(options): options;
-        __options.path = 'https://' + __options.host + __options.path;
-        __options.host = proxy.host.split(':')[0];
-        __options.port = proxy.port;
-        if (_debug) {
-            console.log('=== https-proxy.js begin debug ===');
-            //console.log(JSON.stringify(__options, null, 2));
-            console.log(__options);
-            console.log('=== https-proxy.js end debug ===');
-        }
+        var __options = genOptions(options);
         request = oldhttpsreq.call(null, __options, callback);
         request.on('error', function(e) {
             console.log('problem with request: ' + e.message);
@@ -32,16 +39,7 @@ if (!!process.env.http_proxy){
         var request;
 
         if(_debug) console.log('proxied http request.');
-        var __options = typeof(options)==='string'? url.parse(options): options;
-        __options.path = 'http://' + __options.host + __options.path;
-        __options.host = proxy.host.split(':')[0];
-        __options.port = parseInt(proxy.port);
-        if (_debug) {
-            console.log('=== http-proxy.js begin debug ===');
-            //console.log(JSON.stringify(__options, null, 2));
-            console.log(__options);
-            console.log('=== http-proxy.js end debug ===');
-        }
+        var __options = genOptions(options);
         request = oldhttpreq.call(null, __options, function(res){
             if(_debug) console.log(['callback:', res]);
             callback(res);
